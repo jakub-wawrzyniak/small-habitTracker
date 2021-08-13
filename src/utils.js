422cbffdc316bf50bce0cfcf50a9f0c516183ddf
Jakub Wawrzyniak <jakubwawrzyniak6@gmail.com>
2021-08-13 16:30:32 +0200

    Improved id handeling, AsyncStorage is fully functional

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,27 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 class Habit {
-  static habitIdCounter = 0;
-  constructor(isNew=false) {
+  static idCounter = 0;
+  static usedIds = new Set()
+  constructor(id=Habit.getNewId()) {
     this.title = "New Habit"
     this.description = "Your description"
     this.isDone = false
     this.isMissed = false
-    if (isNew) {
-      this.id = Habit.habitIdCounter;
-      Habit.habitIdCounter++;
-    }
+    this.id = id
+    Habit.usedIds.add(id)
+  }
+
+  static getNewId() {
+    while (Habit.usedIds.has(Habit.idCounter))
+      Habit.idCounter++;
+    return Habit.idCounter;
   }
 
   copy() {
-    return Object.assign(new Habit(), this)
+    return Object.assign(new Habit(this.id), this)
   }
+
 }
 
 class Data {
@@ -48,7 +54,6 @@ class Data {
   async saveHabits() {
     const json = JSON.stringify(this.#habits)
     await AsyncStorage.setItem('habits', json)
-    // console.log("Save successful:", json)
   }
   
   async loadDate(dateStamp) {
@@ -69,7 +74,7 @@ class Data {
     const data = this.#loadedDates[dateStamp]
 
     const habits = this.#habits.map(h => {
-      const habit = Object.assign(new Habit(), h)
+      const habit = Object.assign(new Habit(h.id), h)
       if (habit.id in data) {
         const [isDone, isMissed] = data[habit.id]
         habit.isDone = isDone, habit.isMissed = isMissed
@@ -80,7 +85,7 @@ class Data {
   }
 
   addHabit() {
-    const habit = new Habit(true)
+    const habit = new Habit()
     const core = {
       id: habit.id,
       title: habit.title,
@@ -110,7 +115,7 @@ class Data {
   }
 }
 
-AsyncStorage.clear()
+// AsyncStorage.clear()
 
 const data = new Data()
 export {
